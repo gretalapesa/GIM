4306271e6b0b91b180a61a782301ebccaa00d9a8
gretalapesa <167759056+gretalapesa@users.noreply.github.com>
2024-06-17 02:17:11 +0200

    crea

--- Esercizio_3A/orologio_analogico_prova/sketch.js
+++ Esercizio_3A/orologio_analogico_prova/sketch.js
@@ -13,21 +13,25 @@ function draw() {
     // Disegna il supporto centrale
     noStroke();
     fill(0);
-    ellipse(0, 0, 45);
+    ellipse(0, 0, 40);
 
     // Disegna la barra con i numeri
     drawNumberBar();
 
-    // Calcola gli angoli basati sull'ora corrente
-    let hourAngle = -TAU / 12 * (hour() % 12) - TAU / 12 / 60 * minute() - TAU / 12 / 3600 * second();
-    let minuteAngle = -TAU / 60 * minute() - TAU / 60 / 60 * second();
-    let secondAngle = -TAU / 60 * second();
+    // Calcola gli angoli basati sull'ora corrente con un offset per far oscillare la spirale
+    let hourOffset = PI / 30;  // Offset per le ore
+    let minuteOffset = PI / 30;  // Offset per i minuti
+    let secondOffset = PI / 30;  // Offset per i secondi
+
+    let hourAngle = -PI / 6 * (hour() % 12) - PI / 360 * minute() - PI / 21600 * second() + hourOffset;
+    let minuteAngle = -PI / 30 * minute() - PI / 1800 * second() + minuteOffset;
+    let secondAngle = -PI / 30 * second() + secondOffset;
 
     // Disegna le spirali
     noFill();
     stroke(0);
     strokeWeight(4);
-    drawSpiral(hourAngle, 31);  // Spirale per le ore
+    drawSpiral(hourAngle, 30);  // Spirale per le ore
 
     strokeWeight(2);
     drawSpiral(minuteAngle, 31);  // Spirale per i minuti
@@ -39,28 +43,28 @@ function draw() {
 }
 
 function drawNumberBar() {
-    // Barra nera
+    // Sposta la barra nera più in alto
     noStroke();
     fill(0);
-    rect(-150, -10, 300, 40);  // Centrare la barra nera
+    rect(0, -20, 300, 40);  // Inizia la barra nera al centro e spostala verso l'alto
 
     // Numeri delle ore
     fill(255);
     textSize(16);
     textAlign(CENTER, CENTER);
     for (let i = 1; i <= 12; i++) {
-        text(i, -150 + 25 * (i - 1) + 15, 0);  // Centrare i numeri delle ore
+        text(i, 25 * (i - 1) + 15, 10);  // Centrare i numeri delle ore e spostarli verso l'alto
     }
 
     // Numeri dei minuti (moltiplicati per 5)
     for (let i = 1; i <= 12; i++) {
-        text(i * 5, -150 + 25 * (i - 1) + 15, 20);  // Centrare i numeri dei minuti
+        text(i * 5, 25 * (i - 1) + 15, -7);  // Centrare i numeri dei minuti e spostarli verso l'alto
     }
 }
 
 function drawSpiral(angle, size) {
     beginShape();
-    for (let t = 100; t > 0; t--) {  // Invertire la direzione della spirale
+    for (let t = 0; t < 100; t++) {  // Modificato per far iniziare la spirale dal centro
         let theta = 0.1 * t;
         let x = (0.1 + size * theta) * cos(theta + angle);  // Modificato l'angolo
         let y = (0.1 + size * theta) * sin(theta + angle);  // Modificato l'angolo
